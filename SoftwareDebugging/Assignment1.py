2aa996d15ebdd79a6fbad3f5de1d31b846d9671b
CraigTLennon <CraigTLennon@gmail.com>
2017-03-14 14:43:42 -0400

    lesson 1 notes

---
+++ SoftwareDebugging/Assignment1.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 14 13:52:05 2017
+
+@author: cLennon
+"""
+#The first problem set involves building a debugger
+
+import sys
+
+stepping = True #indicates whether we are stepping line by line through the program or
+                # running the program
+                
+breakpoint={} #points at which to stop to examine variable values (dict line $ to values)
+
+
+
+                
